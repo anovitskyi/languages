6fea4b6892de499704d53f5c73293c734895992b
Andrew Novitskyi <novitskiyandrew@gmail.com>
2019-05-02 23:05:55 +0200

    hello world added

--- src/main/java/com/ano/laguages/controller/LanguageController.java
+++ src/main/java/com/ano/laguages/controller/LanguageController.java
@@ -1,9 +1,14 @@
 package com.ano.laguages.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LanguageController {
 
+    @GetMapping(value = "/say-hello", produces = "text/plain")
+    public String sayHello() {
+        return "Hello world";
+    }
 
 }

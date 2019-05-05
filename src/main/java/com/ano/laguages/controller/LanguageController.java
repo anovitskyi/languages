b46c7fc01052960cfe0b2fb23bf1ad7aa7334d37
Andrew Novitskyi <novitskiyandrew@gmail.com>
2019-05-05 13:49:54 +0200

    removed sensitive information

--- src/main/java/com/ano/laguages/controller/LanguageController.java
+++ src/main/java/com/ano/laguages/controller/LanguageController.java
@@ -1,6 +1,5 @@
 package com.ano.laguages.controller;
 
-import com.ano.laguages.utils.BotAPI;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,12 +17,6 @@ public class LanguageController {
     public String sayHello() {
         log.info("Say hello method launched");
         return "Hello world";
-    }
-
-    @GetMapping(value = "getMessages", produces = "application/json")
-    public String getMessages() {
-        log.info("Retrieving list of anono_bot messages");
-        return restTemplate.getForEntity(BotAPI.getUpdates(), String.class).getBody();
     }
 
 }

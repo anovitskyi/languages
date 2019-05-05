f63de643a5ebd28e842e933ee79a58dd03372dfd
Andrew Novitskyi <novitskiyandrew@gmail.com>
2019-05-05 10:27:52 +0200

    logback added

--- src/main/java/com/ano/laguages/controller/LanguageController.java
+++ src/main/java/com/ano/laguages/controller/LanguageController.java
@@ -1,14 +1,17 @@
 package com.ano.laguages.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class LanguageController {
 
     @GetMapping(value = "/say-hello", produces = "text/plain")
     public String sayHello() {
-        return "Hello world 2";
+        log.info("Say hello method launched");
+        return "Hello world";
     }
 
 }
